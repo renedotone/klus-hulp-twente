93d1003cb808814c42539456a20baa581c8ecdd0
JeroenArneWichers <jwichers@hotmail.com>
2019-02-08 15:59:11 +0100

    user registration and login. Shielded adding klussen only for loged in users

---
+++ resources/views/klusvraagplaatsen.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <form action="">
+        <input type="text" placeholder="naam"><br>
+        <input type="text" placeholder="type hulp vraag"><br>
+        <input type="text" placeholder="beschrijving gevraagde hulp"><br>
+        <input type="button" value="Voeg de hulpvraag toe">
+    </form>
+</html>
