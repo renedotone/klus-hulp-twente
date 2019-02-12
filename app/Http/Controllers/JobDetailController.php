d2928faa26f2a261b3cd2df68eaf2ed0bc1058c2
BernardG7500 <45844379+BennieBen@users.noreply.github.com>
2019-02-12 16:05:36 +0100

    detail layout fixed

--- app/Http/Controllers/JobDetailController.php
+++ app/Http/Controllers/JobDetailController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use Auth;
+use App\Jobs;
 
 class JobDetailController extends Controller
 {
     public function index()
     {
-        return view ('jobdetail');
+        $id= Auth::user()->id;
+        $jobs = DB::table('jobs')->where('id', $id)->get();
+
+        // $jobs = DB::first()->get()
+
+
+        return view('jobdetail')->with('jobs', $jobs);
     }
 }
--- resources/views/jobdetail.blade.php
+++ resources/views/jobdetail.blade.php
@@ -1,1 +1,9 @@
-jobdetail
+@extends('layout')
+
+@section('title', 'Mijn Klusportaal | Details')
+
+@section('content')
+
+jobdetail beschrijving:
+
+@endsection
