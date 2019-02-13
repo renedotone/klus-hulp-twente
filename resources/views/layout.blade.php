76cd3998310d26ee2945d34056ea5b826ab4e42f
BernardG7500 <45844379+BennieBen@users.noreply.github.com>
2019-02-13 14:37:38 +0100

    header tag

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,82 +7,83 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    
+
     <!-- My own CSS -->
     <link href="{{ asset('css/layoutstyle.css') }}" rel="stylesheet" type="text/css" >
 
     <!-- Font -->
-    <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
 
     <title>@yield('title', 'Klus Hulp Twente')</title>
 
   </head>
   <body>
       {{-- <h1 class="title">test</h1> --}}
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li class="nav-item">
-            <a class="nav-link" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/">Over ons</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/">Spelregels</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/">Succesverhalen</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/jobs">Openstaande klussen</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/addingjobs">Voeg een klus toe</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/dashboard">Mijn klusportaal</a>
-        </li>
-        </ul>
-        <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
-            @guest
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    <a class="nav-link" href="/">Home</a>
                 </li>
-                @if (Route::has('register'))
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                    </li>
-                @endif
-            @else
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Over ons</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Spelregels</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Succesverhalen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/jobs">Openstaande klussen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/addingjobs">Voeg een klus toe</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/dashboard">Mijn klusportaal</a>
+                </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
 
-            @endguest
+                    @endguest
 
-        </ul>
-    </div>
-    </nav>
+                </ul>
+            </div>
+        </nav>
+    </header>
 
     <div class="jumbotron jumbotron-fluid custom-jumbotron">
         <div class="container">
@@ -132,5 +133,5 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    
+
   </body>
