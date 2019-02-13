ca0ffce2e1235b2e4fbad1afb6f1f8f30c962fb7
ClemensJongma <clemensjongma@gmail.com>
2019-02-13 14:15:30 +0100

    Titelschaduw

--- app/Http/Controllers/JobDetailController.php
+++ app/Http/Controllers/JobDetailController.php
@@ -3,20 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use DB;
-use Auth;
-use App\Jobs;
 
 class JobDetailController extends Controller
 {
     public function index()
     {
-        $id= Auth::user()->id;
-        $jobs = DB::table('jobs')->where('id', $id)->get();
-
-        // $jobs = DB::first()->get()
-
-
-        return view('jobdetail')->with('jobs', $jobs);
+        return view ('jobdetail');
     }
 }
