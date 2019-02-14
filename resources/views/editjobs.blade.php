c4735e9e2612c50ec73426ed1c7c9cbaa0a4fa75
renedotone <rmspijker@gmail.com>
2019-02-14 13:46:18 +0100

    added basic edit form

--- resources/views/editjobs.blade.php
+++ resources/views/editjobs.blade.php
@@ -4,8 +4,27 @@
 
 @section('content')
 
-<h1>Klus aanpassen</h1>
 
+<div class="container">
+  
+        <h1 class="title">U kunt hier uw klusvraag aanpassen</h1>
+        
+        <form method="#" action="#">
+            {{ csrf_field() }}
+    
+                <div class="form-group ">
+                    <label for="typehulpvraag">Type Hulpvraag</label>
+                    <input name="type_hulpvraag" type="text" class="form-control" id="typehulpvraag" placeholder="type hulp vraag"><br>       
+                </div>
+            
+                <div class="form-group">
+                    <label for="beschrijvinghulpvraag">Beschrijving Hulpvraag</label>
+                    <input name="beschrijving_hulpvraag" type="text" class="form-control" id="beschrijvinghulpvraag" placeholder="beschrijving gevraagde hulp"><br>
+                </div>
+            
+                <button type="submit" class="btn btn-primary">Pas de klusvraag aan</button>
+        </form>
+    </div>
 
 
 @endsection
