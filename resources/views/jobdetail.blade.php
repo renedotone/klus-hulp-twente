6157dc4db23ff4bdc7a1706122094e00e587cbe2
renedotone <rmspijker@gmail.com>
2019-02-20 10:44:11 +0100

    small changes view

--- resources/views/jobdetail.blade.php
+++ resources/views/jobdetail.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-
+<h1>Klusvraag beschrijving</h1>
 
 <div class="container-fluid">
         <div class="row">
@@ -12,51 +12,54 @@
                 <div class="table-responsive">
                     <table class="table table-dark table-hover">
                         <thead>
-                        <tr>
-                            <th>Klusvraag beschrijving</th>
-                        </tr>
-                        
-                        <tr>
-                            <th scope="col">Naam van de plaatser</th>
-                            <th scope="col">Type hulpvraag</th>
-                            <th scope="col">Beschrijving hulpvraag</th>
-                            <th scope="col">Email vrager</th>
-                            <th scope="col">Adres vrager</th>
-                            <th scope="col">Postcode vrager</th>
-                            <th scope="col">Gemeente vrager</th>
-                            <th scope="col">Telefoonnummer vrager</th>
-                            <th scope="col">Reageren?</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Naam van de plaatser</th>
+                                <td>{{ $job->naam_plaatser }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Type hulpvraag</th>
+                                <td>{{ $job->type_hulpvraag }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Beschrijving hulpvraag</th>
+                                <td>{{ $job->beschrijving_hulpvraag }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Email vrager</th>
+                                <td>{{ $job->email_plaatser }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Adres vrager</th> 
+                                <td>{{ $job->adres_plaatser }}</td>  
+                            </tr>
+                            <tr>
+                                <th scope="col">Postcode vrager</th>
+                                <td>{{ $job->postcode_plaatser }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Gemeente vrager</th>
+                                <td>{{ $job->gemeente_plaatser }}</td>
+                            </tr> 
+                            <tr>
+                                <th scope="col">Telefoonnummer vrager</th>
+                                <td>{{ $job->telefoonnummer_plaatser }}</td>
+                            </tr>
+                            <tr>
+                                <th><button class="btn btn-info" onclick="myFunction()">Reageer</button></th>
+                            </tr>
+                            <tr>
+                                <td id="demo"></td>
+                            </tr>
                         </thead>
-                       
-                        <tbody>
-                        <tr>
-                            <td>{{ $job->naam_plaatser }}</td>
-                            <td>{{ $job->type_hulpvraag }}</td>
-                            <td>{{ $job->beschrijving_hulpvraag }}</td>
-                            <td>{{ $job->email_plaatser }}</td>
-                            <td>{{ $job->adres_plaatser }}</td>
-                            <td>{{ $job->postcode_plaatser }}</td>
-                            <td>{{ $job->gemeente_plaatser }}</td>
-                            <td>{{ $job->telefoonnummer_plaatser }}</td>
-                            <td><button class="btn btn-info" onclick="myFunction()">Reageer</button></td>
-                        </tr>
-                        <tr>
-                            <td id="demo"></td>
-
-                        </tr>
-                        </tbody>
-                       
                     </table>
                 </div>
             </div>
         </div>
     </div>
-    <p ></p>
 @endsection
 
 <script>
     function myFunction() {
       document.getElementById("demo").innerHTML = "Bedankt voor uw reactie. De vrager neemt zo snel mogelijk contact met u op.";
     }
-    </script>
+</script>
