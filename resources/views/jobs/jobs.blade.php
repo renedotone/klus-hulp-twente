218c6b802c560d7baa165f7405d4710b318babf7
renedotone <rmspijker@gmail.com>
2019-03-05 14:13:08 +0100

    css done

--- resources/views/jobs/jobs.blade.php
+++ resources/views/jobs/jobs.blade.php
@@ -11,9 +11,13 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>  
 
-                 
- 
 
+<div class="jumbotron jumbotron-fluid custom-jumbotron img-fluid">
+    <div class="container">
+        <h1 class="display-3">Openstaande klussen</h1>
+    </div>
+</div>                 
+ 
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm m-5">
@@ -27,11 +31,11 @@
                                     <div class="input-group">
                                         <input type="search" name="search" class="form-control" placeholder="Zoek in de klussen...">
                                         <div class="input-group-append">
-                                            <button class="btn btn-primary" type="submit">Zoek</button>
+                                            <button id="searchbtn" class="btn" type="submit">Zoek</button>
                                         </div>
                                     </div>    
                                 </th> 
-                                <th scope="col"><a class="btn btn-success" href="{{ url('/jobs') }}">Alle klussen</a></th>
+                                <th scope="col"><a id="allbtn" class="btn" href="{{ url('/jobs') }}">Alle klussen</a></th>
                                 <th scope="col"></th>
                                 <th scope="col"></th> 
                                 <th scope="col"></th> 
@@ -39,11 +43,11 @@
                         </tr>
 
                         <tr>
-                            <th scope="col">Naam van de plaatser</th>
-                            <th scope="col">Type hulpvraag</th>
-                            <th scope="col">Beschrijving hulpvraag</th>
-                            <th scope="col">Gemeente</th>
-                            <th scope="col">Details</th>
+                            <th scope="col" id="colname">Naam van de plaatser</th>
+                            <th scope="col" id="colname">Type hulpvraag</th>
+                            <th scope="col" id="colname">Beschrijving hulpvraag</th>
+                            <th scope="col" id="colname">Gemeente</th>
+                            <th scope="col" id="colname">Details</th>
                         </tr>
                     </thead>
                     
@@ -54,7 +58,7 @@
                             <td>{{ $job->type_hulpvraag }}</td>
                             <td>{{ $job->beschrijving_hulpvraag }}</td>
                             <td>{{ $job->gemeente_plaatser }}</td>
-                            <td><a class="btn btn-info" href="{{ url('/jobdetail', $job->id) }}">Details</a></td>
+                            <td><a id="detailbtn" class="btn" href="{{ url('/jobdetail', $job->id) }}">Details</a></td>
                         </tr>
                     </tbody>
                     @endforeach
